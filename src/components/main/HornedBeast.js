11e8bac8657633f3301d139979a56f8d51c5163e
bc <benchoe@2six.org>
2022-05-11 04:12:45 -0400

    more responsive

--- src/components/main/HornedBeast.js
+++ src/components/main/HornedBeast.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import 'bootstrap';
-import './HornedBeast.css';
+import './style.css';
 
 export default class HornedBeast extends React.Component {
   constructor(props) {
@@ -27,12 +27,12 @@ export default class HornedBeast extends React.Component {
           <Card.Subtitle>{`#${this.props.keyword}`}</Card.Subtitle>
           <Card.Img variant="top" src={this.props.imageUrl} className="card-image img-fluid hover-shadow" />
           <Card.Text>{this.props.description}</Card.Text>
-          <div>
-            <Button variant="primary" onClick={this.faveBeast}>{'Favorite Beast'}</Button>
-            <span>{"❤️"}</span>
-            <span>{`${this.state.clicks} votes`}</span>
-          </div>
         </Card.Body>
+        <div className="card-favorite">
+          <Button variant="primary" onClick={this.faveBeast}>{'Favorite Beast'}</Button>
+          <span className="emoji">{"❤️"}</span>
+          <span className="votes">{`${this.state.clicks} votes`}</span>
+        </div>
       </Card>
     );
   }
