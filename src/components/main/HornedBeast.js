81ba9b810e9def4870f39032a49242e9ed9efba4
bc <benchoe@2six.org>
2022-05-12 01:06:34 -0400

    draft submit

--- src/components/main/HornedBeast.js
+++ src/components/main/HornedBeast.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, Button } from 'react-bootstrap';
-import 'bootstrap';
-import './style.css';
 
-export default class HornedBeast extends React.Component {
+import 'bootstrap';
+import '../../style.css';
+
+export default class HornedBeast extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,16 +19,25 @@ export default class HornedBeast extends React.Component {
     console.log(this.state.clicks);
   };
 
+  // selectBeast = () => {
+  //   this.state={
+  //     key: {}
+  //   }
+  // }
+
   render() {
     return (
       <Card>
         <Card.Body>
           <Card.Title>{this.props.title}</Card.Title>
           <Card.Subtitle>{`#${this.props.keyword}`}</Card.Subtitle>
-          <Card.Img
+          <Card.Img 
             variant="top"
             src={this.props.imageUrl}
+            title={this.props.title}
+            alt={this.props.sourceKey}
             className="card-image img-fluid hover-shadow"
+            onClick={this.selectBeast}
           />
           <Card.Text>{this.props.description}</Card.Text>
         </Card.Body>
@@ -35,8 +45,10 @@ export default class HornedBeast extends React.Component {
           <Button variant="primary" onClick={this.faveBeast}>
             {'Favorite Beast'}
           </Button>
-          <span className="emoji">{'❤️'}</span>
-          <span className="votes">{`${this.state.clicks} votes`}</span>
+          <div className="div-votes">
+            <span className="emoji">{'❤️'}</span>
+            <span className="votes">{`${this.state.clicks}`}</span>
+          </div>
         </div>
       </Card>
     );
