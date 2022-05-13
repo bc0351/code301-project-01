483ab4ef5d1526509eaa1476cac67fe4e7064b32
bc <benchoe@2six.org>
2022-05-13 01:18:28 -0400

    lab03-submit02

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import HornedBeast from './HornedBeast';
-import '../../style.css';
+import '../../css/style.css';
 
 export default class Main extends Component {
   render() {
     let beasts = this.props.data.map((e, i) => {
-      return (
-        <HornedBeast
-          key={i}
-          sourceKey={e._id}
-          title={e.title}
-          imageUrl={e.image_url}
-          description={e.description}
-          keyword={e.keyword}
-          horns={e.horns}
-        />
-      );
+      let props = {
+        key: e._id,
+        title: e.title,
+        imageUrl: e.image_url,
+        description: e.description,
+        keyword: e.keyword,
+        horns: e.horns,
+        handleShow: this.props.handleShow,
+      };
+      return <HornedBeast key={i} beast={props} />;
     });
     return <main>{beasts}</main>;
   }
